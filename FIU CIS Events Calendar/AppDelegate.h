732dc072862ded2ae0d52d98f1109f03a0f1cde7
Ebby Wahman <ebbywahman@gmail.com>
2014-03-26 16:48:16 -0400

    Initial Commit

---
+++ FIU CIS Events Calendar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FIU CIS Events Calendar
+//
+//  Created by Ebby Wahman on 3/26/14.
+//  Copyright (c) 2014 Ebby Wahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
