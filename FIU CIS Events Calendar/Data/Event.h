2300c4fcb97f481af2758c9b5b5706bb97a979bc
Ebby Wahman <ebbywahman@gmail.com>
2014-04-11 11:34:19 -0400

    resuming work

---
+++ FIU CIS Events Calendar/Data/Event.h
@@ -0,0 +1,13 @@
+//
+//  Event.h
+//  FIU CIS Events Calendar
+//
+//  Created by Ebby Wahman on 4/10/14.
+//  Copyright (c) 2014 Ebby Wahman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Event : NSObject
+
+@end
---
+++ FIU CIS Events Calendar/Data/Event.m
@@ -0,0 +1,13 @@
+//
+//  Event.m
+//  FIU CIS Events Calendar
+//
+//  Created by Ebby Wahman on 4/10/14.
+//  Copyright (c) 2014 Ebby Wahman. All rights reserved.
+//
+
+#import "Event.h"
+
+@implementation Event
+
+@end
