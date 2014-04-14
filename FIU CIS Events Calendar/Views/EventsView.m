a363255119bbe6e15997babb258b29aad38158ae
Ebby Wahman <ebbywahman@gmail.com>
2014-04-13 21:46:29 -0400

    Revert 724d8d8..0625eab
    This rolls back to commit 724d8d82c3c88f6d1236b6b911816c92d2bb907d.

--- FIU CIS Events Calendar/Views/EventsView.m
+++ FIU CIS Events Calendar/Views/EventsView.m
@@ -12,7 +12,6 @@
 #import "Events.h"
 #import "EventCellTableViewCell.h"
 #import "AppDelegate.h"
-#import "TestView.h"
 
 #define appDelegate(z) [(AppDelegate *)[[UIApplication sharedApplication] delegate] z]
 
@@ -180,17 +179,9 @@ heightForHeaderInSection:(NSInteger)section{
     if(!noEvents){
     Event *theEvent = [currentEvents objectAtIndex:[indexPath row]];
     NSLog(@"Date for this Event is: %@",[theEvent eventTimeAndDate]);
-<<<<<<< HEAD
     [theEvent setAddedToUser:[NSNumber numberWithBool:YES]];
     }
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-=======
-    
-    TestView *theTestView = [[TestView alloc] initWithEvent:theEvent];
-    
-    [self.navigationController pushViewController:theTestView animated:YES];
-    
->>>>>>> FETCH_HEAD
 }
 
 //- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
